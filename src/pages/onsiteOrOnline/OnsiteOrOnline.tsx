883d343de4ac3ad3c2d605ac61820ff5b85ad562
Karine Débora Wagner <karinedwagner@gmail.com>
2025-08-13 16:08:05 -0400

    refatora o componente OnsiteOrOnline

---
+++ src/pages/onsiteOrOnline/OnsiteOrOnline.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import Automonia from "../../assets/assets/automonia.png";
+import Comunicacao from "../../assets/assets/comunicacao.png";
+import Proatividade from "../../assets/assets/proatividade.png";
+import Resiliencia from "../../assets/assets/resiliencia.png";
+import Companherismo from "../../assets/assets/companherismo.png";
+
+const OnsiteOrOnline: React.FC = () => {
+    const data = [
+        { icon: Automonia, text: "Abordagem com Resultados Científicos", alt: "Ícone de autonomia" },
+        { icon: Comunicacao, text: "Acompanhamento Profissional", alt: "Ícone de comunicação" },
+        { icon: Proatividade, text: "50 minutos", alt: "Ícone de proatividade" },
+        { icon: Resiliencia, text: "Ferramentas de Apoio", alt: "Ícone de resiliência" },
+        { icon: Companherismo, text: "Individual e Particular", alt: "Ícone de companheirismo" },
+    ];
+
+    return (
+        <section className="text-left max-w-screen-lg mx-auto px-2 py-4 bg-bege rounded-xl shadow-md">
+            <h2 className="titleSection text-center">
+                Atendimentos<span> - Presencial/Online </span>
+            </h2>
+            <div className="flex flex-col gap-4 mt-2">
+                {data.map((item, index) => (
+                    <div 
+                        key={index}
+                        className="flex gap-4 items-center w-full border-t  border-terracota pt-4"
+                    >
+                        <img 
+                            src={item.icon} 
+                            alt={item.alt}
+                            className="ml-2 h-12 w-2"
+                        />
+                        <p className="text-marrom text-lg md:text-lg w-full">{item.text}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default OnsiteOrOnline;
