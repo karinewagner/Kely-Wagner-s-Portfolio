35ade4bea97c8936394d94980ddf611c652c9c10
Karine Débora Wagner <karinedwagner@gmail.com>
2025-08-13 16:03:33 -0400

    cria padrão para chamar os componentes

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,29 +1,45 @@
-import "../assets/css/style.css";
+import "../styles/globals.css";
 
 import Navigation from "./navigation/Navigation";
-import Home from "./home/Home";
-import About from "./about/About";
-import Benefits from "./benefits/Benefits";
-import AreaOfExpertise from "./areaOfExpertise/AreaOfExpertise";
-import MyAppointments from "./myAppointments/MyAppointments";
-import OnsiteOrOnline from "./onsiteOrOnline/OnsiteOrOnline";
-import Contact from "./contact/Contact";
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import MyApproach from "../pages/myApproach/MyApproach";
+import Benefits from "../pages/benefits/Benefits";
+import MyAppointments from "../pages/myAppointments/MyAppointments";
+import OnsiteOrOnline from "../pages/onsiteOrOnline/OnsiteOrOnline";
+import Contact from "../pages/contact/Contact";
 import Footer from "./footer/Footer";
 
 const MyApp: React.FC = () => {
     return (
         <div className="tudo">
             <Navigation />
-            <Home />
-            <About />
-            <Benefits />
-            <AreaOfExpertise />
-            <MyAppointments />
-            <OnsiteOrOnline />
-            <Contact />
+            <main>
+                <section id="home">
+                    <Home />
+                </section>
+                <section id="about">
+                    <About />
+                </section>
+                <section id="myApproach">
+                    <MyApproach />
+                </section>
+                <section id="benefits">
+                    <Benefits />
+                </section>
+                <section id="appointments">
+                    <MyAppointments />
+                </section>
+                <section id="onsite-or-online">
+                    <OnsiteOrOnline />
+                </section>
+                <section id="contact">
+                    <Contact />
+                </section>
+            </main>
             <Footer />
         </div>
     );
-}
+};
 
 export default MyApp;
